b8cca17ebb685bccb65e63e5e89683a1e7a43cfa
Jack Linhart <iatenine@gmail.com>
2021-08-25 21:30:00 -0600

    Created GenerateHtml Module

---
+++ static/js/generateHtml.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const utils = require("util");
+
+// Promisify readFile
+const readFileAsync = utils.promisify(fs.readFile);
+
+class GenerateHtml {
+  constructor(teamMembers) {
+    this.loadHtmlComponents()
+      .then((htmlComponents) => {
+        this.teamName = teamName;
+        this.teamMembers = teamMembers;
+        this.htmlComponents = htmlComponents;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  loadHtmlComponents = async () => {
+    this.header = await readFileAsync("../html/index.html", "utf8").catch(
+      (err) => console.error(err)
+    );
+    this.footer = await readFileAsync("../html/footer.html", "utf8").catch(
+      (err) => console.error(err)
+    );
+  };
+
+  generateMemberCard = (member, role) => {
+    switch (role) {
+      case "Engineer":
+        member["color"] = "bg-primary";
+        member["special"] = `${member.github}`;
+        member["specialLabel"] = "Github";
+        break;
+      case "Intern":
+        member["color"] = "bg-danger";
+        member["special"] = `${member.school}`;
+        member["specialLabel"] = "School";
+        break;
+      default:
+        member["color"] = "bg-success";
+        member["special"] = `${member.officeNumber}`;
+        member["specialLabel"] = "Office Number";
+    }
+    return `<div class="card m-4 text-light">
+      <div class="card-header ${member.color}">
+        <h3>${member.name}</h3>
+            <div class="d-flex flex-row">
+                <p class="m-2">${member.role}</p>
+            </div>
+      </div>
+      <ul class="list-group list-group-flush text-dark">
+        <li class="list-group-item">ID: ${member.id}</li>
+        <li class="list-group-item">
+            Email: <a href="mailto:${member.email}" target="no_blank">${member.email}</a>
+          </li>
+        <li class="list-group-item">${member.specialLabel} = ${member.special}</li>
+      </ul>
+    </div>`;
+  };
+}
+
+module.exports = GenerateHtml;
